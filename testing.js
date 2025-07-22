75095ea79723987cede081218c98c9c44b0c3eb3
rajateshr <rajateshr@gmail.com>
2025-07-22 12:58:13 +0530

    Added new files

---
+++ testing.js
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Sample JavaScript Example</title>
+    <script>
+        // This function runs when the button is clicked
+        function sayHello() {
+            alert("Hello! Welcome to JavaScript.");
+        }
+    </script>
+</head>
+<body>
+
+    <h1>My JavaScript Demo</h1>
+    <button onclick="sayHello()">Click Me!</button>
+
+</body>
+</html>
